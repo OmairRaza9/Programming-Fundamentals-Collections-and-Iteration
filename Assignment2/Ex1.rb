fa3bdc67a30ea9c70e59026db99dab064bd69e49
Omair <omairraza9@gmail.com>
2017-09-20 22:39:53 -0400

    ex2 part 2 completed

--- Assignment2/Ex1.rb
+++ Assignment2/Ex1.rb
@@ -22,6 +22,34 @@ bands = ['ssb', 'lupe', 'ratm']
 fav_colours = [:black, :orange, :yellow, :green, :brown]
 
 
+#part2
+
+definitions = {
+  :shofar => "A ram's horn blown as a wind instrument, sounded in Biblical times chiefly to communicate signals in battle and announce certain religious occasions and in modern times chiefly at synagogue services on Rosh Hashanah and Yom Kippur.",
+  :instrument => "a contrivance or apparatus for producing musical sounds",
+  :electronic => "of or relating to electronics or to devices, circuits, or systems developed through electronics."
+}
+
+fav_movies ={
+  :cruel_intentions => 1999,
+  :drunk_wedding => 2015,
+  :vicky_christina_barcelona => 2008
+}
+
+cities = {
+  :toronto => 2600000,
+  :vancouver => 603500,
+  :seoul => 10580000
+}
+
+names_ages = {
+  :ari => 29,
+  :meg => 27,
+  :sarah => 23,
+  :chris => 21,
+  :sarah_cousin => 28
+}
+
 #####Exercise 1
 #Print out the array of coin flips.
 
@@ -42,4 +70,28 @@ age << 0
 
 #Using the hash of movie information, access and print the year of one of the movies.
 
-movies = [fight_club, 1997]
+movies = {
+"Fight_club" => "2004",
+"Hangover" => "2007",
+"Remember the Titans" => "2000"
+}
+
+#puts "The winner of best picture was #{movies['Hangover']}"
+#end
+
+
+#fav_colours = ['black', 'orange', 'yellow', 'green', 'brown']
+
+
+
+#ex 2
+#Print out the last element of the array of your favourite colours.
+#puts fav_colours[fav_colours.length -1]
+
+
+#2 Add a new city to the hash of cities and population.
+
+cities['newyork'] = 987654321
+
+puts cities
+end
